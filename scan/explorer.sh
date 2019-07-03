dcc53ecec5ee764ebdc1dd0a4131d7eb09809134
zteeed <zteeed@minet.net>
2019-07-03 22:14:40 +0200

    Force result file to be empty on start

--- scan/explorer.sh
+++ scan/explorer.sh
@@ -1,7 +1,8 @@
 function main() {
   user="duboc_au"
   gateway="ssh.imtbs-tsp.eu"
-  for i in {211..254}; do
+  echo '' > ./result.txt
+  for i in {2..254}; do
       result=$(dig -x 157.159.15.$i +nocomments +noquestion +noauthority +noadditional +nostats | grep PTR | awk '{print $5}')
       if [ -z $result ]; then continue; fi # if result is None
       hostname=$(ssh -i $PWD/keys/id_rsa -o BatchMode=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o ConnectTimeout=2 $user@157.159.15.$i hostname)

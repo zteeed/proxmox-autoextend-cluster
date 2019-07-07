8a5ff91d1c5cdda0c6d15b7caa47ce4635e85753
zteeed <zteeed@minet.net>
2019-07-07 19:52:31 +0200

    Add scripts to tell ansible what ip is available for the VM

---
+++ ansible/scripts/new_ip/get_new_ip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# n=$(pvecm status | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1 | cut -d'.' -f4)
+n=$(head -n 1 ./num.txt)
+echo "172.16.0.$n"
+echo "$(( $n + 1))" > ./num.txt

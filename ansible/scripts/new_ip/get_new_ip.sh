0d8fd4d117ed57ce160fd3dc6e5c805bfb6c20c5
zteeed <zteeed@minet.net>
2019-07-07 19:54:22 +0200

    Manage if file num.txt does not exist

--- ansible/scripts/new_ip/get_new_ip.sh
+++ ansible/scripts/new_ip/get_new_ip.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # n=$(pvecm status | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | tail -n 1 | cut -d'.' -f4)
+if [ ! -f "./num.txt" ]; then
+  ./reset.sh
+fi
 n=$(head -n 1 ./num.txt)
 echo "172.16.0.$n"
 echo "$(( $n + 1))" > ./num.txt

a36c85cb9cf24a64779f2d9c7c11827f8222d936
zteeed <zteeed@minet.net>
2019-07-10 20:19:49 +0200

    Improve reset script (use last ip from pvecm)

--- ansible/scripts/new_ip/reset.sh
+++ ansible/scripts/new_ip/reset.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 path=$(dirname "${BASH_SOURCE[0]}")
+num=$(pvecm status | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | tail -n 1 | cut -d'.' -f4)
+num=$(( num + 1))
 file="$path/num.txt"
-echo '90' > $file
+echo $num > $file
